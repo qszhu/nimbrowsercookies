09f0a63ec6f662a24e54ca3a644a081eefc0e900
Qinsi ZHU <qszhu2006@gmail.com>
2024-06-07 15:49:15 +0800

    fix path

--- src/nimbrowsercookies.nim
+++ src/nimbrowsercookies.nim
@@ -11,8 +11,6 @@ export chrome, firefox, types
 proc readCookies*(browser: Browser, profilePath, host: string): StringTableRef =
   case browser
   of Browser.FIREFOX:
-    let dbFn = profilePath / "cookies.sqlite"
-    readCookiesFromFirefox(dbFn, host)
+    readCookiesFromFirefox(profilePath, host)
   of Browser.CHROME:
-    let dbFn = profilePath / "Cookies"
-    readCookiesFromChrome(dbFn, host)
+    readCookiesFromChrome(profilePath, host)
